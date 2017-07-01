d24f94de5a68e5b232576dafd9eab1f40d0b4703
Paragen <k-paradox-p@mail.ru>
2017-07-01 16:50:47 +0300

    fix 9000

--- proxy.h
+++ proxy.h
@@ -60,7 +60,7 @@ class Proxy {
                                                     ds(&storage) {}
 
         ~Node() {
-           if (isClient) {
+           /*if (isClient) {
                 cout << "Client drop";
                 if (address != "") {
                     cout << address;
@@ -68,7 +68,7 @@ class Proxy {
                 cout << "\n";
             } else {
                 cout << "Server drop\n";
-            }
+            }*/
             ds->release(buffer.release());
             if (!crutch) {
                 socket.close();
@@ -90,7 +90,7 @@ class Proxy {
     void onErrorSlot(Socket &socket);
 
 public:
-    static const unsigned BUFFER_SIZE = 4 * 1024, STARTED_POOL = 10;
+    static const unsigned BUFFER_SIZE = 10 * 1024 * 1024, STARTED_POOL = 10;
 
     Proxy() : dataStorage(STARTED_POOL, BUFFER_SIZE) {
         server.setSlot(boost::bind(&Proxy::onListenSlot, this, _1), socketMode::toListen);
@@ -110,7 +110,7 @@ public:
     }
 
     void connect(Node &node) {
-        cout << "Connect to " + node.address + " by " +  (node.address == "80" ? "HTTP" : "HTTPS") +  "\n";
+        //cout << "Connect to " + node.address + " by " +  (node.address == "80" ? "HTTP" : "HTTPS") +  "\n";
         unique_ptr<Node> tmpPtr;
         SocketWrap socketWrap;
 

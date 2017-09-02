2aec21c27bdd28a7661d63731f501b4aca412c9a
Paragen <k-paradox-p@mail.ru>
2017-09-02 16:33:01 +0300

    change signal

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main(int argc, char** argv) {
     sa.sa_sigaction = &my_handler;
     sigset_t ss;
     sigemptyset(&ss);
-    sigaddset(&ss,SIGINT);
+    sigaddset(&ss,SIGTSTP);
     sa.sa_mask = ss;
     sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGINT,&sa,NULL);
+    sigaction(SIGTSTP,&sa,NULL);
 
  
     proxy.run(string(argv[1]),string(argv[2]));
